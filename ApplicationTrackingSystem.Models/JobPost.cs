87cf51ca52781b91f3286e1737b5dda7765198ab
qdevelopergithub <43872892+qdevelopergithub@users.noreply.github.com>
2024-05-24 18:11:15 +0530

    detail

--- ApplicationTrackingSystem.Models/JobPost.cs
+++ ApplicationTrackingSystem.Models/JobPost.cs
@@ -19,16 +19,22 @@ namespace ApplicationTrackingSystem.Models
         public DateTime FromDate { get; set; }
         [DataType(DataType.Date)]
         public DateTime ToDate { get; set; }
-      
-        public string Description { get; set; }
+        [Required]
+        public string? CandidateSkill { get; set; }
+        [Required]
+        public string? Experience {  get; set; }
+        [Required]
+        public string? Qualification { get; set; }
+        [Required]      
+        public string? Description { get; set; }
         [DataType(DataType.Date)]
         public DateTime? CreatedAt { get; set; }
         [StringLength(100)]
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
         [DataType(DataType.Date)]
         public DateTime? ModifiedAt { get; set; }
         [StringLength(100)]
-        public string ModifiedBy { get; set; }
+        public string? ModifiedBy { get; set; }
 
     }
 }
